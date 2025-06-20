6046afcf295225f96992c29a22f1cdedfa82c03a
Venkatesh-boot <venkatesh.net5@gmail.com>
2025-06-20 09:48:41 +0530

    gateway service updated new packages

--- services/order-service/src/main/java/com/ecomm/project/order_service/OrderServiceApplication.java
+++ services/order-service/src/main/java/com/ecomm/project/order_service/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.ecomm.project.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- services/order-service/src/main/java/com/ecomm/project/order_service/controller/OrderController.java
+++ services/order-service/src/main/java/com/ecomm/project/order_service/controller/OrderController.java
@@ -2,12 +2,14 @@ package com.ecomm.project.order_service.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 
-    @GetMapping("/orders/list")
+    @GetMapping("/list")
     public ResponseEntity<String> getOrders() {
         return ResponseEntity.ok("This is from order service.");
     }
