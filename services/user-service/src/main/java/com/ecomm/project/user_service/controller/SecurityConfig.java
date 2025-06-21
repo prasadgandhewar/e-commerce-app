1a2f1e1095641358383fd5d6ba58fdaa6e2e5fae
Prasad <prasad.gandhewar@yahoo.ca>
2025-06-21 18:37:04 +0530

    added eureka

--- services/user-service/src/main/java/com/ecomm/project/user_service/controller/SecurityConfig.java
+++ services/user-service/src/main/java/com/ecomm/project/user_service/controller/SecurityConfig.java
@@ -1,24 +1,24 @@
-package com.ecomm.project.user_service.controller;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/public/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
-
-        return http.build();
-    }
-}
+//package com.ecomm.project.user_service.controller;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authz -> authz
+//                        .requestMatchers("/public/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+//
+//        return http.build();
+//    }
+//}
--- services/user-service/src/main/java/com/ecomm/project/user_service/controller/UserController.java
+++ services/user-service/src/main/java/com/ecomm/project/user_service/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.ecomm.project.user_service.controller;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableDiscoveryClient
 public class UserController {
 
     @GetMapping("/users/list")
