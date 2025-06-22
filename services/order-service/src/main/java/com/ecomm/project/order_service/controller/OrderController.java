bbbbe6dca58ea89435f126e0beb02c29a2156b75
Venkatesh-boot <venkatesh.net5@gmail.com>
2025-06-22 13:33:44 +0530

    removed security config and moved config to config server

--- services/order-service/src/main/java/com/ecomm/project/order_service/controller/OrderController.java
+++ services/order-service/src/main/java/com/ecomm/project/order_service/controller/OrderController.java
@@ -1,16 +1,22 @@
 package com.ecomm.project.order_service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
+    @Value("${user.count}")
+    String count;
+
     @GetMapping("/list")
     public ResponseEntity<String> getOrders() {
-        return ResponseEntity.ok("This is from order service.");
+        return ResponseEntity.ok("This is from order service count - "+ count);
     }
 }
--- services/product-service/src/main/java/com/ecomm/project/product_service/controller/ProductController.java
+++ services/product-service/src/main/java/com/ecomm/project/product_service/controller/ProductController.java
@@ -1,14 +1,19 @@
 package com.ecomm.project.product_service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ProductController {
+    @Value("${user.count}")
+    String count;
 
     @GetMapping("/products/list")
     public ResponseEntity<String> getProducts() {
-        return ResponseEntity.ok("This is from product service.");
+        return ResponseEntity.ok("This is from product service- " + count);
     }
 }
