7162b0e6289c674e3852d7bbfe08b1ca67947fc4
Prasad <prasad.gandhewar@yahoo.ca>
2025-06-22 10:43:38 +0530

    added config sever

--- services/user-service/src/main/java/com/ecomm/project/user_service/controller/UserController.java
+++ services/user-service/src/main/java/com/ecomm/project/user_service/controller/UserController.java
@@ -1,17 +1,25 @@
 package com.ecomm.project.user_service.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @EnableDiscoveryClient
 public class UserController {
 
+    @Value("${user.count}")
+    String count;
+
     @GetMapping("/users/list")
     public ResponseEntity<String> getUsers() {
-        return new ResponseEntity<>("This is user method", HttpStatus.OK);
+
+
+        return new ResponseEntity<>("This is user method and count is - " + count, HttpStatus.OK);
     }
 }
