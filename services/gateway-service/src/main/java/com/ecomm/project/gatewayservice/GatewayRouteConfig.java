f3544481cac8051558b9d518f6c1b83d1889638a
Prasad <prasad.gandhewar@yahoo.ca>
2025-07-16 19:20:38 +0530

    changes to run

--- services/gateway-service/src/main/java/com/ecomm/project/gatewayservice/GatewayRouteConfig.java
+++ services/gateway-service/src/main/java/com/ecomm/project/gatewayservice/GatewayRouteConfig.java
@@ -1,19 +1,19 @@
-package com.ecomm.project.gatewayservice;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GatewayRouteConfig {
-
-    @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p.path("/orders/**").uri("http://order-service:8200"))
-                .route(p -> p.path("/users/**").uri("http://user-service:8000"))
-                .route(p -> p.path("/products/**").uri("http://product-service:8300"))
-                .build();
-    }
-}
+//package com.ecomm.project.gatewayservice;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GatewayRouteConfig {
+//
+//    @Bean
+//    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p.path("/orders/**").uri("http://order-service:8200"))
+//                .route(p -> p.path("/users/**").uri("http://user-service:8000"))
+//                .route(p -> p.path("/products/**").uri("http://product-service:8300"))
+//                .build();
+//    }
+//}
--- services/order-service/src/main/java/com/ecomm/project/order_service/proxy/ProductServiceProxy.java
+++ services/order-service/src/main/java/com/ecomm/project/order_service/proxy/ProductServiceProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service", url = "http://localhost:8300")
 public interface ProductServiceProxy {
 
     @GetMapping("/products/list")
